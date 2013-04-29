d301ab0040efe03c71f74ecff16c65c8ca784ca6
Javier <javier.gomez.corio@gmail.com>
2013-04-29 13:23:03 +0200

    Add basic icons to cpanel

--- component/admin/views/cpanel/tmpl/default.php
+++ component/admin/views/cpanel/tmpl/default.php
@@ -1,13 +1,14 @@
 <?php
 /**
- * @package   AdminTools
- * @copyright Copyright (c)2010-2013 Nicholas K. Dionysopoulos
- * @license   GNU General Public License version 3, or later
- * @version   $Id$
+ * @package     RedDesign.Component
+ * @subpackage  Administrator
+ *
+ * @copyright   Copyright (C) 2013 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later, see LICENSE.
  */
 
 // Protect from unauthorized access
-defined('_JEXEC') or die();
+defined('_JEXEC') or die;
 
 JHtml::_('behavior.framework');
 JHtml::_('behavior.modal');
@@ -16,19 +17,47 @@ $lang = JFactory::getLanguage();
 $option = 'com_reddesign';
 ?>
 
-
-<h2><?php echo JText::_('COM_REDDESING_CPANEL') ?></h2>
-
-
-<div class="icon">
-	<a href="index.php?option=<?php echo $option ?>&view=help">
-		<img
-			src="<?php echo rtrim(JURI::base(), '/'); ?>/../media/com_reddesign/assets/images/help32.png"
-			border="0" alt="<?php echo JText::_('COM_REDDESING_CPANEL_HELP_ICON') ?>"/>
+<div id="cpanel" class="span12">
+	<div class="icon">
+		<a href="index.php?option=<?php echo $option ?>&view=designtypes">
+			<img
+				src="<?php echo rtrim(JURI::base(), '/'); ?>/../media/com_reddesign/assets/images/reddesign_designarea_48.png"
+				border="0" alt="<?php echo JText::_('COM_REDDESING_CPANEL_ICON_DESGIGNTYPE_ALT') ?>"/>
 				<span>
-					<?php echo JText::_('COM_REDDESING_CPANEL_HELP_ICON') ?><br/>
+					<?php echo JText::_('COM_REDDESING_CPANEL_ICON_DESGIGNTYPE') ?><br/>
 				</span>
-	</a>
+		</a>
+	</div>
+	<div class="icon">
+		<a href="index.php?option=<?php echo $option ?>&view=fonts">
+			<img
+				src="<?php echo rtrim(JURI::base(), '/'); ?>/../media/com_reddesign/assets/images/reddesign_fonts_48.png"
+				border="0" alt="<?php echo JText::_('COM_REDDESING_CPANEL_ICON_FONTS_ALT') ?>"/>
+				<span>
+					<?php echo JText::_('COM_REDDESING_CPANEL_ICON_FONTS') ?><br/>
+				</span>
+		</a>
+	</div>
+	<div class="icon">
+		<a href="index.php?option=<?php echo $option ?>&view=help">
+			<img
+				src="<?php echo rtrim(JURI::base(), '/'); ?>/../media/com_reddesign/assets/images/reddesign_help_48.png"
+				border="0" alt="<?php echo JText::_('COM_REDDESING_CPANEL_ICON_HELP_ALT') ?>"/>
+				<span>
+					<?php echo JText::_('COM_REDDESING_CPANEL_ICON_HELP') ?><br/>
+				</span>
+		</a>
+	</div>
+	<div class="icon">
+		<a href="index.php?option=<?php echo $option ?>&view=configuration">
+			<img
+				src="<?php echo rtrim(JURI::base(), '/'); ?>/../media/com_reddesign/assets/images/reddesign_configuration_48.png"
+				border="0" alt="<?php echo JText::_('COM_REDDESING_CPANEL_ICON_CONFIGURATION_ALT') ?>"/>
+				<span>
+					<?php echo JText::_('COM_REDDESING_CPANEL_ICON_CONFIGURATION') ?><br/>
+				</span>
+		</a>
+	</div>
 </div>
 
 <div style="clear: both;"></div>
