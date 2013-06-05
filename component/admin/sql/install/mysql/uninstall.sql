83b90145eaf39d2af51ce6826859dbd23a3e50fd
Javier <javier.gomez.corio@gmail.com>
2013-06-05 18:09:54 +0200

    Update the uninstall .sql file

--- component/admin/sql/install/mysql/uninstall.sql
+++ component/admin/sql/install/mysql/uninstall.sql
@@ -1,12 +1,4 @@
-DROP TABLE IF EXISTS `#__reddesign_area`;
-DROP TABLE IF EXISTS `#__reddesign_designtype`;
-DROP TABLE IF EXISTS `#__reddesign_image`;
-DROP TABLE IF EXISTS `#__reddesign_redshop`;
-DROP TABLE IF EXISTS `#__reddesign_order`;
-DROP TABLE IF EXISTS `#__reddesign_template`;
-DROP TABLE IF EXISTS `#__reddesign_config`;
-
 DROP TABLE IF EXISTS `#__reddesign_fonts`;
 DROP TABLE IF EXISTS `#__reddesign_designs`;
 DROP TABLE IF EXISTS `#__reddesign_designbackgrounds`;
-DROP TABLE IF EXISTS `#__reddesign_backgroundfont`;
+DROP TABLE IF EXISTS `#__reddesign_backgrounds_fonts`;
