678f0a6e52bc10e1cc18291f5c54c9d8ebc790bc
Javier <javier.gomez.corio@gmail.com>
2013-06-10 19:18:43 +0200

    Default structure for frontend view
    I had to cheat the FoF a bit to get a View without database table. See https://github.com/akeeba/fof/wiki/Creating%20a%20view%20without%20a%20database%20table

---
+++ component/site/views/reddesign/tmpl/default.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package     RedDesign.Component
+ * @subpackage  Site
+ *
+ * @copyright   Copyright (C) 2013 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later, see LICENSE.
+ */
+
+defined('_JEXEC') or die;
+?>
+
+ToDo
