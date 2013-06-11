518dbca8992d48e9c57cd206098d08ba3f7f3aba
Javier <javier.gomez.corio@gmail.com>
2013-06-11 18:22:27 +0200

    Add a new view that shows a background

---
+++ component/site/views/designbackground/tmpl/default.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package     RedDesign.Component
+ * @subpackage  Site
+ *
+ * @copyright   Copyright (C) 2013 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later, see LICENSE.
+ */
+
+defined('_JEXEC') or die;
+?>
+
+<img class="left" id="background"
+				     src="<?php echo FOFTemplateUtils::parsePath('media://com_reddesign/assets/backgrounds/') . $this->item->jpegpreviewfile; ?>"/>
+
+
+</form>
+
