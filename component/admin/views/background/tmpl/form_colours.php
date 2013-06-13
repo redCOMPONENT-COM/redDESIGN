1f7fd1956d9ef450062f47c5dc8e26c5ab263fb2
Javier <javier.gomez.corio@gmail.com>
2013-06-13 11:01:28 +0200

    Add jPicker to the tmpl

--- component/admin/views/background/tmpl/form_colours.php
+++ component/admin/views/background/tmpl/form_colours.php
@@ -9,6 +9,27 @@
 
 defined('_JEXEC') or die();
 
+FOFTemplateUtils::addJS('media://com_reddesign/jpicker/jpicker-1.1.6.min.js');
+FOFTemplateUtils::addCSS('media:///com_reddesign/jpicker/css/jPicker-1.1.6.css');
+FOFTemplateUtils::addCSS('media:///com_reddesign/jpicker/jPicker.css');
 
 ?>
 
+<script type="text/javascript">
+	akeeba.jQuery(document).ready(
+		function()
+		{
+			akeeba.jQuery('.Multiple').jPicker.defaults.images.clientPath='<?php echo JURI::root() ?>media/com_reddesign/jpicker/images/';
+			akeeba.jQuery('.Multiple').jPicker();
+		});
+</script>
+<div>
+	<p>
+		<input class="Multiple" type="text" value="000000" /><br />
+	</p>
+	<p>
+		<a class="btn" href="#">
+			<?php echo JText::_('COM_REDDESIGN_BACKGROUND_COLOURS_ADD_COLOR'); ?>
+		</a>
+	</p>
+</div>
