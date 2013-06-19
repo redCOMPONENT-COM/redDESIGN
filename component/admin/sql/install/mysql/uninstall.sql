7a0fdb1c51dce6eddff436e5266b044072e46c6c
Javier <javier.gomez.corio@gmail.com>
2013-06-19 12:07:54 +0200

    Rename images to backgrouns on uninstall sql file

--- component/admin/sql/install/mysql/uninstall.sql
+++ component/admin/sql/install/mysql/uninstall.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS `#__reddesign_configs`;
 DROP TABLE IF EXISTS `#__reddesign_designtypes`;
 DROP TABLE IF EXISTS `#__reddesign_fonts`;
 DROP TABLE IF EXISTS `#__reddesign_font_chars`;
-DROP TABLE IF EXISTS `#__reddesign_images`;
+DROP TABLE IF EXISTS `#__reddesign_backgrounds`;
 DROP TABLE IF EXISTS `#__reddesign_orders`;
 DROP TABLE IF EXISTS `#__reddesign_redshops`;
 DROP TABLE IF EXISTS `#__reddesign_templates`;
