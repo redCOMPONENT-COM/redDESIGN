e4e768c782bd63ebb7cb4e8d28765ebcf45e2cbd
bnisevic <bojan@redcomponent.com>
2013-06-21 14:14:36 +0200

    - finished Font Edit view - fixed some issues found by PHP CodeSniffer - fixed some bugs

--- component/admin/sql/install/mysql/uninstall.sql
+++ component/admin/sql/install/mysql/uninstall.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS `#__reddesign_areas`;
 DROP TABLE IF EXISTS `#__reddesign_configs`;
 DROP TABLE IF EXISTS `#__reddesign_designtypes`;
 DROP TABLE IF EXISTS `#__reddesign_fonts`;
-DROP TABLE IF EXISTS `#__reddesign_font_chars`;
+DROP TABLE IF EXISTS `#__reddesign_chars`;
 DROP TABLE IF EXISTS `#__reddesign_images`;
 DROP TABLE IF EXISTS `#__reddesign_orders`;
 DROP TABLE IF EXISTS `#__reddesign_redshops`;
