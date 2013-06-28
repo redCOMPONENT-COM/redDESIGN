54a57a8c82a88f3cb6af8169f504a6bbcb15161a
Javier <javier.gomez.corio@gmail.com>
2013-06-28 13:53:25 +0200

    Allow to select the Production Background using stars

--- component/admin/views/designtype/tmpl/form_backgrounds.php
+++ component/admin/views/designtype/tmpl/form_backgrounds.php
@@ -33,69 +33,105 @@ defined('_JEXEC') or die;
 			akeeba.jQuery('#uploadBgForm').fadeIn("slow");
 		}
 	</script>
-
-	<table id="itemsList" class="table table-striped">
-		<thead>
-		<tr>
-			<th width="9%">
-				<?php echo JText::_('ID'); ?>
-			</th>
-			<th>
-				<?php echo JText::_('COM_REDDESIGN_DESIGNTYPE_BACKGROUNDS_EPS_FILE'); ?>
-			</th>
-			<th width="9%">
-				<?php echo JText::_('COM_REDDESIGN_DESIGNTYPE_BACKGROUNDS_PRODUCTION_BACKGROUND'); ?>
-			</th>
-			<th width="9%">
-				<?php echo JText::_('JPUBLISHED'); ?>
-			</th>
-			<th width="115">
-				<?php echo JText::_('COM_REDDESIGN_COMMON_REMOVE'); ?>
-			</th>
-		</tr>
-		</thead>
-		<tbody>
-		<?php if ($count = count($this->backgrounds)) : ?>
-			<?php $i = -1;
-			$m       = 1; ?>
-			<?php foreach ($this->backgrounds as $background) : ?>
-				<?php
-				$i++;
-				$m                     = 1 - $m;
-				$background->published = $background->enabled;
-				?>
-				<tr class="<?php echo 'row' . $m; ?>">
-					<td>
-						<?php echo $background->reddesign_background_id; ?>
-					</td>
-					<td align="left">
-						<a class="modal"
-						   href="<?php echo FOFTemplateUtils::parsePath('media://com_reddesign/assets/backgrounds/' . $background->image_path); ?>">
-							<strong><?php echo $this->escape(JText::_($background->title)) ?></strong>
-						</a>
-					</td>
-					<td align="center" width="9%">
-						@ToDo
-					</td>
-					<td align="center" width="9%">
-						<?php echo JHTML::_('grid.published', $background, $i); ?>
-					</td>
-					<td>
-						<?php // @ToDo: this delete background feature still needs to be implemented. It wil also have to remove areas, fonts and colors ?>
-						<button type="button" class="btn btn-danger delete">
-							<i class="icon-minus icon-white"></i>
-							<span><?php echo JText::_('COM_REDDESIGN_DESIGNTYPE_BACKGROUNDS_REMOVE'); ?></span>
-						</button>
+	<form id="backgrounds_form" name="backgrounds" method="post" action="index.php">
+		<input type="hidden" name="<?php echo JFactory::getSession()->getFormToken(); ?>" value="1"/>
+		<input type="hidden" name="option" value="com_reddesign">
+		<input type="hidden" name="view" value="backgrounds">
+		<input type="hidden" name="task" id="backgrounds_task" value="">
+		<input type="hidden" name="backgrounds_reddesign_background_id"
+			   id="backgrounds_reddesign_background_id"
+			   value="">
+		<input type="hidden" name="backgrounds_reddesign_designtype_id"
+			   id="backgrounds_reddesign_designtype_id"
+			   value="<?php echo $this->item->reddesign_designtype_id; ?>">
+		<table id="itemsList" class="table table-striped">
+			<thead>
+			<tr>
+				<th width="9%">
+					<?php echo JText::_('ID'); ?>
+				</th>
+				<th>
+					<?php echo JText::_('COM_REDDESIGN_DESIGNTYPE_BACKGROUNDS_EPS_FILE'); ?>
+				</th>
+				<th width="9%">
+					<?php echo JText::_('COM_REDDESIGN_DESIGNTYPE_BACKGROUNDS_PRODUCTION_BACKGROUND'); ?>
+				</th>
+				<th width="9%">
+					<?php echo JText::_('JPUBLISHED'); ?>
+				</th>
+				<th width="115">
+					<?php echo JText::_('COM_REDDESIGN_COMMON_REMOVE'); ?>
+				</th>
+			</tr>
+			</thead>
+			<tbody>
+			<?php if ($count = count($this->backgrounds)) : ?>
+				<?php $i = -1;
+				$m       = 1; ?>
+				<?php foreach ($this->backgrounds as $background) : ?>
+					<?php
+					$i++;
+					$m                     = 1 - $m;
+					$background->published = $background->enabled;
+					?>
+					<tr class="<?php echo 'row' . $m; ?>">
+						<td>
+							<?php echo $background->reddesign_background_id; ?>
+						</td>
+						<td align="left">
+							<a class="modal"
+							   href="<?php echo FOFTemplateUtils::parsePath('media://com_reddesign/assets/backgrounds/' . $background->image_path); ?>">
+								<strong><?php echo $this->escape(JText::_($background->title)) ?></strong>
+							</a>
+						</td>
+						<td align="center" width="9%" class="switchBg">
+							<?php if (!$background->isPDFbgimage) : ?>
+								<a class="jgrid" href="javascript:void(0);"
+									onclick="setPDFbg('<?php echo $background->reddesign_background_id; ?>')" >
+									<span class="state notdefault">
+										<span class="text">Default</span>
+									</span>
+								</a>
+							<?php else : ?>
+								<a class="jgrid">
+									<span class="state default">
+										<span class="text">Default</span>
+									</span>
+								</a>
+							<?php endif; ?>
+						</td>
+						<td align="center" width="9%">
+							<?php echo JHTML::_('grid.published', $background, $i); ?>
+						</td>
+						<td>
+							<?php // @ToDo: this delete background feature still needs to be implemented. It wil also have to remove areas, fonts and colors ?>
+							<button type="button" class="btn btn-danger delete">
+								<i class="icon-minus icon-white"></i>
+								<span><?php echo JText::_('COM_REDDESIGN_COMMON_REMOVE'); ?></span>
+							</button>
+						</td>
+					</tr>
+				<?php endforeach ?>
+			<?php else : ?>
+				<tr>
+					<td colspan="5">
+						<?php echo JText::_('COM_REDDESIGN_COMMON_NORECORDS') ?>
 					</td>
 				</tr>
-			<?php endforeach ?>
-		<?php else : ?>
-			<tr>
-				<td colspan="5">
-					<?php echo JText::_('COM_REDDESIGN_COMMON_NORECORDS') ?>
-				</td>
-			</tr>
-		<?php endif; ?>
-		</tbody>
-	</table>
+			<?php endif; ?>
+			</tbody>
+		</table>
+	</form>
 </div>
+
+<script type="text/javascript">
+	function setPDFbg(bgid) {
+		var backgrounds_reddesign_background_id;
+
+		backgrounds_reddesign_background_id = bgid;
+
+		akeeba.jQuery('#backgrounds_task').val('setPDFbg');
+		akeeba.jQuery('#backgrounds_reddesign_background_id').val(bgid);
+		akeeba.jQuery('#backgrounds_form').submit();
+	}
+</script>
