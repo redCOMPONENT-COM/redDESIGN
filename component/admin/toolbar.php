34551e322b2fab5440a873951bd435c0e36cec72
bnisevic <bojan@redcomponent.com>
2013-07-13 02:35:55 +0200

    Fixed #184 Fixde #185

--- component/admin/toolbar.php
+++ component/admin/toolbar.php
@@ -19,16 +19,53 @@ defined('_JEXEC') or die;
 class ReddesignToolbar extends FOFToolbar
 {
 	/**
-	 * Prepares the toolbar for Cpanel view
+	 * Prepares the toolbar for Designtype view
 	 *
 	 * @return void
 	 */
-	public function onCpanelsBrowse()
+	public function onBrowse()
 	{
-		// Set the toolbar title
-		JToolBarHelper::title(JText::_('COM_REDDESIGN_CPANEL_TITLE_DASHBOARD'), 'reddesign');
+		parent::onBrowse();
+
+		$view = $this->input->getCmd('view', 'cpanel');
+		JToolBarHelper::title(JText::_('COM_REDDESIGN') . ' - ' . ucfirst($view),  $view);
 
 		// Add Components options (see config.xml)
+		JToolBarHelper::divider();
+		JToolBarHelper::preferences('com_reddesign');
+	}
+
+	/**
+	 * Prepares the toolbar for Designtype view
+	 *
+	 * @return void
+	 */
+	public function onEdit()
+	{
+		parent::onEdit();
+
+		$view = $this->input->getCmd('view', 'cpanel');
+		JToolBarHelper::title(JText::_('COM_REDDESIGN') . ' - ' . ucfirst($view), $view);
+
+		// Add Components options (see config.xml)
+		JToolBarHelper::divider();
+		JToolBarHelper::preferences('com_reddesign');
+	}
+
+	/**
+	 * Prepares the toolbar for Designtype view
+	 *
+	 * @return void
+	 */
+	public function onAdd()
+	{
+		parent::onAdd();
+
+		$view = $this->input->getCmd('view', 'cpanel');
+		JToolBarHelper::title(JText::_('COM_REDDESIGN') . ' - ' . ucfirst($view), $view);
+
+		// Add Components options (see config.xml)
+		JToolBarHelper::divider();
 		JToolBarHelper::preferences('com_reddesign');
 	}
 }
