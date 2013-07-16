b31b44f7db7950c4628396a0f7fd5224ece42130
Ushma Shah <ushma@redcomponent.com>
2013-07-16 11:47:03 +0530

    FrontDesignTypeList

--- component/site/views/designtypes/tmpl/default.php
+++ component/site/views/designtypes/tmpl/default.php
@@ -10,16 +10,29 @@
 defined('_JEXEC') or die();
 ?>
 <div class="form-horizontal">
-    <?php foreach($this->items as $designtype) : ?>
-    <div class="control-group">
-        <div class="control-label">
-           <img id="dsigntype"
-				 src="<?php echo FOFTemplateUtils::parsePath('media://com_reddesign/assets/designtypes/thumbnails/') . $designtype->sample_thumb; ?>" class="img-polaroid"/>
-        </div>
+    <?php foreach($this->items as $designtype) :
+			if ($this->params->get('navigation_link'))
+			{
+				/*
+					@Todo: update this link when finished details designtype view
+				*/
+				$link  = JRoute::_('index.php?option=com_reddesign&id=' . $designtype->reddesign_designtype_id);
+			}
+			else
+			{
+				$link  = JRoute::_('index.php?option=com_reddesign&id=' . $designtype->reddesign_designtype_id);
+			}
+	?>
+	<div class="control-group">
+		<div class="control-label">
+			<img id="dsigntype"
+		src="<?php echo FOFTemplateUtils::parsePath('media://com_reddesign/assets/designtypes/thumbnails/') . $designtype->sample_thumb; ?>" class="img-polaroid"/>
+		</div>
 		<label class="controls">
-           <a href="index.php?option=com_reddesign&id=<?php echo $designtype->reddesign_designtype_id ?>"><strong><?php echo $designtype->title; ?></strong></a> <br/> <?php echo $designtype->description; ?>
-        </label>
-    </div>
+			<a href="<?php echo $link ?>"><strong><?php echo $designtype->title; ?></strong></a> <br/> <?php echo substr($designtype->description,0, 150); ?>
+			<p class="readmore"><a href="">Read more: <?php echo $designtype->title; ?></a></p>
+		</label>
+	</div>
     <?php endforeach; ?>
 </div>
 
