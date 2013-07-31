831a6148b04742a15185e0e6695579caa53df3d6
Javier <javier.gomez.corio@gmail.com>
2013-07-31 13:08:58 +0200

    move removal of backgrounds to table delete instead of onAfterDelete

--- component/admin/tables/background.php
+++ component/admin/tables/background.php
@@ -20,14 +20,73 @@ defined('_JEXEC') or die;
 class ReddesignTableBackground extends FOFTable
 {
 	/**
-	 * Removes background related files after erasing the background in database
+	 * Delete a background
 	 *
-	 * @param   int  $oid  background database row id
+	 * @param   integer  $oid  The primary key value of the item to delete
 	 *
-	 * @return bool|void
+	 * @return  boolean  True on success
 	 */
-	protected function onAfterDelete($oid)
+	public function delete($oid = null)
 	{
+		if ($oid)
+		{
+			$this->load($oid);
+		}
+
+		if (!$this->onBeforeDelete($oid))
+		{
+			return false;
+		}
+
+		$k  = $this->_tbl_key;
+		$pk = (is_null($oid)) ? $this->$k : $oid;
+
+		// If no primary key is given, return false.
+
+		if ($pk === null)
+		{
+			throw new UnexpectedValueException('Null primary key not allowed.');
+		}
+
+		// If tracking assets, remove the asset first.
+
+		if ($this->_trackAssets)
+		{
+			// Get and the asset name.
+			$this->$k = $pk;
+			$name     = $this->_getAssetName();
+
+			// Do NOT touch JTable here -- we are loading the core asset table which is a JTable, not a FOFTable
+			$asset    = JTable::getInstance('Asset');
+
+			if ($asset->loadByName($name))
+			{
+				if (!$asset->delete())
+				{
+					$this->setError($asset->getError());
+
+					return false;
+				}
+			}
+			else
+			{
+				$this->setError($asset->getError());
+
+				return false;
+			}
+		}
+
+		// If this resource has tags, delete the tags first
+		if ($this->_has_tags)
+		{
+			if (!$this->_tagsHelper->deleteTagData($this, $pk))
+			{
+				$this->setError('Error deleting Tags');
+
+				return false;
+			}
+		}
+
 		// Delete background EPS
 		if (JFile::exists(JPATH_SITE . '/media/com_reddesign/assets/backgrounds/' . $this->eps_file))
 		{
@@ -46,6 +105,18 @@ class ReddesignTableBackground extends FOFTable
 			JFile::delete(JPATH_SITE . '/media/com_reddesign/assets/backgrounds/thumbnails/' . $this->thumbnail);
 		}
 
-		parent::onAfterDelete($oid);
+		// Delete the row by primary key.
+		$query = $this->_db->getQuery(true);
+		$query->delete();
+		$query->from($this->_tbl);
+		$query->where($this->_tbl_key . ' = ' . $this->_db->q($pk));
+		$this->_db->setQuery($query);
+
+		// Check for a database error.
+		$this->_db->execute();
+
+		$result = $this->onAfterDelete($oid);
+
+		return $result;
 	}
 }
