0685a57812c2c98163a104e9c152b5e4d992cb20
bnisevic <bojan@redcomponent.com>
2013-08-09 12:37:34 +0200

    Fixed wrong JInput use.

--- component/admin/controllers/designtypes.php
+++ component/admin/controllers/designtypes.php
@@ -77,7 +77,7 @@ class ReddesignControllerDesigntypes extends FOFController
 			}
 
 			// If no thumbnail has been uploaded and user has checked the generate thumbnail based on uploaded image
-			if (empty($thumbFile['name']) && $this->input->getBool('autoGenerateThumbCheck'))
+			if (empty($thumbFile['name']) && $this->input->getBool('autoGenerateThumbCheck', false))
 			{
 				$dest = JPATH_ROOT . '/media/com_reddesign/assets/designtypes/thumbnails/' . $uploadedImageFile['mangled_filename'];
 				JFile::copy($uploadedImageFile['filepath'], $dest);
