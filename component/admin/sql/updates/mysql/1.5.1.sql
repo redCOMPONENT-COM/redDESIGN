ca797cef466c3810ce387f81909d956e1a74878d
Javier <javier.gomez.corio@gmail.com>
2013-09-25 16:16:55 +0200

    Update the sql update according to last pull

--- component/admin/sql/updates/mysql/1.5.1.sql
+++ component/admin/sql/updates/mysql/1.5.1.sql
@@ -1,4 +1,6 @@
 ALTER TABLE  `#__reddesign_areas` DROP `fedcolor`;
+ALTER TABLE  `#__reddesign_areas` DROP `apply_cruv`;
+ALTER TABLE  `#__reddesign_areas` DROP `txtangle`;
 
 ALTER TABLE  `#__reddesign_areas` ADD `input_field_type` TINYINT( 3 ) NULL AFTER `maxline`;
-
+ALTER TABLE  `#__reddesign_areas` ADD `default_text` TEXT NULL DEFAULT NULL AFTER  `color_code`
