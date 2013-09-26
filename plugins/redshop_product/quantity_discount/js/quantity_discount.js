998428fb9e3ab60e0ac817fcd524e944b1f5afce
Gunjan Patel <gunjan@redcomponent.com>
2013-09-26 17:09:51 +0530

    Remove docblock and fix akeeba.jQuery

--- plugins/redshop_product/quantity_discount/js/quantity_discount.js
+++ plugins/redshop_product/quantity_discount/js/quantity_discount.js
@@ -1,16 +1,9 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: gunjan
- * Date: 3/29/13
- * Time: 10:33 AM
- * To change this template use File | Settings | File Templates.
- */
-$(function(){
+akeeba.jQuery(function(){
 
     var quantity_elm = $('input[id^="quantity"]');
     quantity_elm.hide();
 
-    $('.quantity_discount_radio').click(function(){
+    akeeba.jQuery('.quantity_discount_radio').click(function(){
 
         var nq = $(this).val();
         var pid = $(this).attr('product_id');
