1cd5c2c67be1d83a6b0fea819b25e5b1baedde35
Gunjan Patel <gunjan@redcomponent.com>
2013-09-30 18:55:07 +0530

    REDDESIGN-402 #comment Fix Display Price issue

--- plugins/redshop_product/quantity_discount/js/quantity_discount.js
+++ plugins/redshop_product/quantity_discount/js/quantity_discount.js
@@ -15,11 +15,11 @@ rsjQuery(function(){
 
     rsjQuery('.quantity_discount_radio').click(function(){
 
-        var nq = rsjQuery(this).val();
-        var pid = rsjQuery(this).attr('product_id');
+        var nq           = rsjQuery(this).val();
+        var pid          = rsjQuery(this).attr('product_id');
 
+        rsjQuery('#main_price' + pid).val(rsjQuery(this).attr('price'));
         quantity_elm.val(nq);
-
         calculateTotalPrice(pid, 0);
     });
 });
