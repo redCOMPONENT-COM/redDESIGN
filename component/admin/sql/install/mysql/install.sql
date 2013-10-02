8a8c7a1999658f4696be2f74ff109356db91b6a4
bnisevic <bojan@redcomponent.com>
2013-10-02 22:03:42 +0200

    Fixed wrong column name in the install.sql

--- component/admin/sql/install/mysql/install.sql
+++ component/admin/sql/install/mysql/install.sql
@@ -159,7 +159,7 @@ CREATE TABLE IF NOT EXISTS `#__reddesign_attribute_mapping` (
   `reddesign_designtype_id` INT(11) NOT NULL,
   `product_id`              INT(11) NOT NULL,
   `property_id`             INT(11) NOT NULL,
-  PRIMARY KEY (`reddesign_designtype_id`, `product_id`, `attribute_id`)
+  PRIMARY KEY (`reddesign_designtype_id`, `product_id`, `property_id`)
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8
