dae7442c949532dc412c30b21d3674267d1ea5e6
bnisevic <bojan@redcomponent.com>
2013-12-26 03:12:02 +0100

    Fixing chars.

--- component/admin/views/font/tmpl/edit_chars.php
+++ component/admin/views/font/tmpl/edit_chars.php
@@ -25,100 +25,105 @@ echo $this->loadTemplate('chars_js');
 
 	<div id="all-rows">
 		<div id="other-rows">
-			<?php foreach($this->item->chars as $charSettings) : ?>
-				<div id="row<?php echo $charSettings->id; ?>" class="row">
-					<div class="control-group character-group">
-						<label for="fontChar<?php echo $charSettings->id; ?>">
-							<?php echo JText::_('COM_REDDESIGN_FONT_CHARACTER') ?>
-						</label>
-						<input class="input-mini"
-							   type="text"
-							   maxlength="1"
-							   value="<?php echo $charSettings->font_char; ?>"
-							   id="fontChar<?php echo $charSettings->id; ?>"
-							   name="fontChar<?php echo $charSettings->id; ?>"
-							>
-					</div>
-					<div class="control-group character-group">
-						<label for="width<?php echo $charSettings->id; ?>">
-							<?php echo JText::_('COM_REDDESIGN_FONT_CHARACTER_WIDTH') ?>
-						</label>
-						<input class="input-small"
-							   type="text"
-							   maxlength="15"
-							   value="<?php echo $charSettings->width; ?>"
-							   id="width<?php echo $charSettings->id; ?>"
-							   name="width<?php echo $charSettings->id; ?>"
-							>
-					</div>
-					<div class="control-group character-group">
-						<label for="height<?php echo $charSettings->id; ?>">
-							<?php echo JText::_('COM_REDDESIGN_FONT_CHARACTER_HEIGHT') ?>
-						</label>
-						<input class="input-small"
-							   type="text"
-							   maxlength="15"
-							   value="<?php echo $charSettings->height; ?>"
-							   id="height<?php echo $charSettings->id; ?>"
-							   name="height<?php echo $charSettings->id; ?>"
-							>
-					</div>
-					<div class="control-group character-group">
-						<label for="typography<?php echo $charSettings->id; ?>"><?php echo JText::_('COM_REDDESIGN_FONT_TYPOGRAPHY') ?></label>
-						<?php
-							echo JHtml::_(
-											'select.genericlist',
-											$this->typographies,
-											'typography' . $charSettings->id,
-											'',
-											'value',
-											'text',
-											$charSettings->typography
-							);
-						?>
-					</div>
-					<div class="control-group character-group">
-						<label for="typographyHeight<?php echo $charSettings->id; ?>">
-							<?php echo JText::_('COM_REDDESIGN_FONT_TYPOGRAPHY_HEIGHT') ?>
-						</label>
-						<input class="input-small"
-							   type="text"
-							   maxlength="15"
-							   value="<?php echo $charSettings->typography_height; ?>"
-							   id="typographyHeight<?php echo $charSettings->id; ?>"
-							   name="typographyHeight<?php echo $charSettings->id; ?>"
-							>
-					</div>
-					<div class="control-group character-group">
-						<label for="add_button_<?php echo $charSettings->id; ?>">&nbsp;</label>
-						<button id="add_button_<?php echo $charSettings->id; ?>"
-								type="button"
-								class="btn btn-success btn-mini"
-								onclick="saveChar(<?php echo $charSettings->id; ?>);"
-								title="<?php echo JText::_('COM_REDDESIGN_FONT_SAVE_CHAR'); ?>">
-							<?php echo JText::_('COM_REDDESIGN_FONT_SAVE_CHAR'); ?>
-						</button>
-					</div>
-					<div class="control-group character-group">
-						<label for="remove_button_<?php echo $charSettings->id; ?>">&nbsp;</label>
-						<button id="remove_button_<?php echo $charSettings->id; ?>"
-								type="button"
-								class="btn btn-danger btn-mini"
-								onclick="removeChar(<?php echo $charSettings->id; ?>);"
-								title="<?php echo JText::_('COM_REDDESIGN_FONT_REMOVE_CHAR'); ?>"
-							>
-							<?php echo JText::_('COM_REDDESIGN_FONT_REMOVE_CHAR'); ?>
-						</button>
-					</div>
-				</div>
-			<?php endforeach; ?>
+			<div class="span9">
+				<?php foreach($this->item->chars as $charSettings) : ?>
+					<?php if (!empty($charSettings)) : ?>
+						<div id="row<?php echo $charSettings->id; ?>" class="row-fluid">
+							<div class="control-group character-group span1">
+								<label for="fontChar<?php echo $charSettings->id; ?>">
+									<?php echo JText::_('COM_REDDESIGN_FONT_CHARACTER') ?>
+								</label>
+								<input class="input-mini"
+									   type="text"
+									   maxlength="1"
+									   value="<?php echo $charSettings->font_char; ?>"
+									   id="fontChar<?php echo $charSettings->id; ?>"
+									   name="fontChar<?php echo $charSettings->id; ?>"
+								>
+							</div>
+							<div class="control-group character-group span1">
+								<label for="width<?php echo $charSettings->id; ?>">
+									<?php echo JText::_('COM_REDDESIGN_FONT_CHARACTER_WIDTH') ?>
+								</label>
+								<input class="input-mini"
+									   type="text"
+									   maxlength="15"
+									   value="<?php echo $charSettings->width; ?>"
+									   id="width<?php echo $charSettings->id; ?>"
+									   name="width<?php echo $charSettings->id; ?>"
+								>
+							</div>
+							<div class="control-group character-group span1">
+								<label for="height<?php echo $charSettings->id; ?>">
+									<?php echo JText::_('COM_REDDESIGN_FONT_CHARACTER_HEIGHT') ?>
+								</label>
+								<input class="input-mini"
+									   type="text"
+									   maxlength="15"
+									   value="<?php echo $charSettings->height; ?>"
+									   id="height<?php echo $charSettings->id; ?>"
+									   name="height<?php echo $charSettings->id; ?>"
+								>
+							</div>
+							<div class="control-group character-group span3">
+								<label for="typography<?php echo $charSettings->id; ?>"><?php echo JText::_('COM_REDDESIGN_FONT_TYPOGRAPHY') ?></label>
+								<?php
+									echo JHtml::_(
+													'select.genericlist',
+													$this->typographies,
+													'typography' . $charSettings->id,
+													'',
+													'value',
+													'text',
+													$charSettings->typography
+									);
+								?>
+							</div>
+							<div class="control-group character-group span2">
+								<label for="typographyHeight<?php echo $charSettings->id; ?>">
+									<?php echo JText::_('COM_REDDESIGN_FONT_TYPOGRAPHY_HEIGHT') ?>
+								</label>
+								<input class="input-small"
+									   type="text"
+									   maxlength="15"
+									   value="<?php echo $charSettings->typography_height; ?>"
+									   id="typographyHeight<?php echo $charSettings->id; ?>"
+									   name="typographyHeight<?php echo $charSettings->id; ?>"
+								>
+							</div>
+							<div class="control-group character-group span2">
+								<label for="addButton<?php echo $charSettings->id; ?>">&nbsp;</label>
+								<button id="addButton<?php echo $charSettings->id; ?>"
+										type="button"
+										class="btn btn-success btn-small"
+										onclick="saveChar(<?php echo $charSettings->id; ?>);"
+										title="<?php echo JText::_('COM_REDDESIGN_FONT_SAVE_CHAR'); ?>"
+								>
+									<?php echo JText::_('COM_REDDESIGN_FONT_SAVE_CHAR'); ?>
+								</button>
+							</div>
+							<div class="control-group character-group span2">
+								<label for="removeButton<?php echo $charSettings->id; ?>">&nbsp;</label>
+								<button id="removeButton<?php echo $charSettings->id; ?>"
+										type="button"
+										class="btn btn-danger btn-small"
+										onclick="removeChar(<?php echo $charSettings->id; ?>);"
+										title="<?php echo JText::_('COM_REDDESIGN_FONT_REMOVE_CHAR'); ?>"
+								>
+									<?php echo JText::_('COM_REDDESIGN_FONT_REMOVE_CHAR'); ?>
+								</button>
+							</div>
+						</div>
+					<?php endif; ?>
+				<?php endforeach; ?>
+			</div>
 		</div>
 		<div id="startRow" class="row-fluid">
 			<div class="span9">
 				<div class="row-fluid">
 					<div class="control-group character-group span1">
-						<label for="font_char"><?php echo JText::_('COM_REDDESIGN_FONT_CHARACTER') ?></label>
-						<input class="input-mini" type="text" value="" maxlength="1" id="font_char" name="font_char">
+						<label for="fontChar"><?php echo JText::_('COM_REDDESIGN_FONT_CHARACTER') ?></label>
+						<input class="input-mini" type="text" value="" maxlength="1" id="fontChar" name="fontChar">
 					</div>
 					<div class="control-group character-group span1">
 						<label for="width"><?php echo JText::_('COM_REDDESIGN_FONT_CHARACTER_WIDTH') ?></label>
