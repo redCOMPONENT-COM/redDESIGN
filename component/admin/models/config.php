523d07d10599b76ae70bcf78bb057ae953538557
thongredweb <thong@redweb.dk>
2014-01-07 17:36:48 +0700

    RD-70: Update configuration view. Configuration class

---
+++ component/admin/models/config.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @package     Reddesign.Backend
+ * @subpackage  Models
+ *
+ * @copyright   Copyright (C) 2012 - 2013 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later, see LICENSE.
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Config Model
+ *
+ * @package     Reddesign.Backend
+ * @subpackage  Models
+ * @since       1.0
+ */
+class ReddesignModelConfig extends RModelAdmin
+{
+	/**
+	 * Method to save the form data.
+	 *
+	 * @param   array  $data  The form data.
+	 *
+	 * @return  boolean  True on success, False on error.
+	 */
+	public function save($data)
+	{
+		$new = array(
+			'id' => 1,
+			'params' => json_encode($data)
+		);
+
+		return parent::save($new);
+	}
+
+	/**
+	 * Method to get a single record.
+	 *
+	 * @param   integer  $pk  The id of the primary key.
+	 *
+	 * @return  mixed  Object on success, false on failure.
+	 */
+	public function getItem($pk = 1)
+	{
+		$table = $this->getTable();
+
+		// Attempt to load the row.
+		$return = $table->load($pk);
+
+		// Check for a table object error.
+		if ($return === false)
+		{
+			return false;
+		}
+
+		// Get the table properties.
+		$properties = $table->getProperties(1);
+
+		// Decode the config.
+		return json_decode($properties['params'], true);
+	}
+}
---
+++ component/admin/views/config/tmpl/edit.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * @package     Reddesign.Backend
+ * @subpackage  Views
+ *
+ * @copyright   Copyright (C) 2012 - 2013 redCOMPONENT.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later, see LICENSE.
+ */
+
+defined('_JEXEC') or die;
+
+$action = JRoute::_('index.php?option=com_reddesign&view=config&layout=edit');
+
+// HTML helpers
+JHtml::_('behavior.keepalive');
+JHtml::_('rbootstrap.tooltip');
+JHtml::_('rjquery.chosen', 'select');
+?>
+
+<form action="<?php echo $action; ?>" method="post" name="adminForm" id="adminForm" class="form-validate form-horizontal">
+	<ul class="nav nav-tabs">
+		<li class="active">
+			<a href="#general" data-toggle="tab">
+				<?php echo JText::_('COM_REDDESIGN_CONFIG_GENERAL_CONFIGURATION'); ?>
+			</a>
+		</li>
+		<li class="">
+			<a href="#images" data-toggle="tab">
+				<?php echo JText::_('COM_REDDESIGN_CONFIG_IMAGES_CONFIGURATION'); ?>
+			</a>
+		</li>
+		<li class="">
+			<a href="#permissions" data-toggle="tab">
+				<?php echo JText::_('JCONFIG_PERMISSIONS_LABEL'); ?>
+			</a>
+		</li>
+	</ul>
+
+	<div id="my-tab-content" class="tab-content">
+		<div class="tab-pane active" id="general">
+			<div class="control-group">
+				<div class="control-label">
+					<?php echo $this->form->getLabel('autoCustomize'); ?>
+				</div>
+				<div class="controls">
+					<?php echo $this->form->getInput('autoCustomize'); ?>
+				</div>
+			</div>
+			<div class="control-group">
+				<div class="control-label">
+					<?php echo $this->form->getLabel('unit'); ?>
+				</div>
+				<div class="controls">
+					<?php echo $this->form->getInput('unit'); ?>
+				</div>
+			</div>
+			<div class="control-group">
+				<div class="control-label">
+					<?php echo $this->form->getLabel('font_preview_text'); ?>
+				</div>
+				<div class="controls">
+					<?php echo $this->form->getInput('font_preview_text'); ?>
+				</div>
+			</div>
+		</div>
+		<div class="tab-pane" id="images">
+			<div class="control-group">
+				<div class="control-label">
+					<?php echo $this->form->getLabel('max_svg_file_size'); ?>
+				</div>
+				<div class="controls">
+					<?php echo $this->form->getInput('max_svg_file_size'); ?>
+				</div>
+			</div>
+			<div class="control-group">
+				<div class="control-label">
+					<?php echo $this->form->getLabel('max_svg_backend_bg_width'); ?>
+				</div>
+				<div class="controls">
+					<?php echo $this->form->getInput('max_svg_backend_bg_width'); ?>
+				</div>
+			</div>
+			<div class="control-group">
+				<div class="control-label">
+					<?php echo $this->form->getLabel('max_svg_backend_bg_height'); ?>
+				</div>
+				<div class="controls">
+					<?php echo $this->form->getInput('max_svg_backend_bg_height'); ?>
+				</div>
+			</div>
+			<div class="control-group">
+				<div class="control-label">
+					<?php echo $this->form->getLabel('max_svg_frontend_bg_width'); ?>
+				</div>
+				<div class="controls">
+					<?php echo $this->form->getInput('max_svg_frontend_bg_width'); ?>
+				</div>
+			</div>
+			<div class="control-group">
+				<div class="control-label">
+					<?php echo $this->form->getLabel('max_svg_frontend_bg_height'); ?>
+				</div>
+				<div class="controls">
+					<?php echo $this->form->getInput('max_svg_frontend_bg_height'); ?>
+				</div>
+			</div>
+			<div class="control-group">
+				<div class="control-label">
+					<?php echo $this->form->getLabel('max_background_thumbnail_width'); ?>
+				</div>
+				<div class="controls">
+					<?php echo $this->form->getInput('max_background_thumbnail_width'); ?>
+				</div>
+			</div>
+			<div class="control-group">
+				<div class="control-label">
+					<?php echo $this->form->getLabel('max_background_thumbnail_height'); ?>
+				</div>
+				<div class="controls">
+					<?php echo $this->form->getInput('max_background_thumbnail_height'); ?>
+				</div>
+			</div>
+			<div class="control-group">
+				<div class="control-label">
+					<?php echo $this->form->getLabel('productionFilePadding'); ?>
+				</div>
+				<div class="controls">
+					<?php echo $this->form->getInput('productionFilePadding'); ?>
+				</div>
+			</div>
+		</div>
+		<div class="tab-pane" id="permissions">
+			<div class="control-group">
+				<div class="control-label">
+					<?php echo $this->form->getLabel('rules'); ?>
+				</div>
+				<div class="controls">
+					<?php echo $this->form->getInput('rules'); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- hidden fields -->
+	<input type="hidden" name="option" value="com_reddesign" />
+	<input type="hidden" name="task" value="" />
+	<input type="hidden" name="id" value="1" />
+	<?php echo JHTML::_('form.token'); ?>
+</form>
