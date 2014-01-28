cce3e5135b6eda59fd64918d5e57bb763f902777
javier gomez <javier.gomez.corio@gmail.com>
2014-01-28 14:17:44 +0100

    Update 2.0.0.4.sql

--- component/admin/sql/updates/mysql/2.0.0.4.sql
+++ component/admin/sql/updates/mysql/2.0.0.4.sql
@@ -6,3 +6,5 @@ ALTER TABLE `#__reddesign_product_mapping` ADD `default_designtype_id` INT(11) N
 ALTER TABLE `#__reddesign_attribute_mapping` DROP PRIMARY KEY;
 ALTER TABLE `#__reddesign_attribute_mapping` CHANGE `reddesign_designtype_id` `designtype_id` INT(11) NOT NULL;
 ALTER TABLE `#__reddesign_attribute_mapping` ADD PRIMARY KEY(`designtype_id`, `product_id`, `property_id`);
+
+SET FOREIGN_KEY_CHECKS = 1;
